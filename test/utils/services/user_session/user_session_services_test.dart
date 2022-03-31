b2711383b08ee3a0867d780477a4109ae59a50e5
Adrian Pop <adi.pop.bv@gmail.com>
2022-03-31 14:20:53 +0300

    MVR-24 Fixed aync gap error in some unit tests

--- test/utils/services/user_session/user_session_services_test.dart
+++ test/utils/services/user_session/user_session_services_test.dart
@@ -1,12 +1,12 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mooover/utils/services/user_session_services.dart';
 
 void main() {
   group("User session services", () {
-    test("refresh token is being saved", () {
+    test("refresh token is being saved", () async {
       WidgetsFlutterBinding.ensureInitialized();
-      UserSessionServices().setRefreshToken("value");
+      await UserSessionServices().setRefreshToken("value");
       expect(UserSessionServices().refreshToken, "value");
     });
   });
